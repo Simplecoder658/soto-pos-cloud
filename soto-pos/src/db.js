5346f50939cc14efcdf4621326ed1e47a052c865
Simplecoder658 <syahmi.ibad.4e.23@gmail.com>
2026-04-26 11:37:03 +0700

    Update db.js

--- soto-pos/src/db.js
+++ soto-pos/src/db.js
@@ -1,35 +1,22 @@
-const URL_API = "https://script.google.com/macros/s/AKfycbyU6Cl0sMw5hlbcX8TxRUBSF1qm0_OxkEFeIMbcieW5v4YFq3jNUwDzGH0UdDBgPZ5r/exec";
+const URL_API = "https://script.google.com/macros/s/AKfycbxsqCieTKiZKEvYi0KfK35hWhosBJrGwFppFTXVDpu9Z-GCalqULKqKqVEWxiv45ALqPw/exec";
 
 export const getInitialData = async () => {
   try {
     const res = await fetch(URL_API);
     return await res.json();
-  } catch (err) {
-    console.error("Gagal mengambil data:", err);
-    return null;
-  }
+  } catch (err) { return null; }
 };
 
 export const saveOrder = async (orderData) => {
   try {
-    const res = await fetch(URL_API, {
-      method: "POST",
-      body: JSON.stringify({ action: "saveOrder", ...orderData }),
-    });
+    const res = await fetch(URL_API, { method: "POST", body: JSON.stringify({ action: "saveOrder", ...orderData }) });
     return await res.json();
-  } catch (err) {
-    return { status: "ERROR" };
-  }
+  } catch (err) { return { status: "ERROR" }; }
 };
 
 export const updateShiftStatus = async (status) => {
   try {
-    const res = await fetch(URL_API, {
-      method: "POST",
-      body: JSON.stringify({ action: "updateShift", status }),
-    });
+    const res = await fetch(URL_API, { method: "POST", body: JSON.stringify({ action: "updateShift", status }) });
     return await res.json();
-  } catch (err) {
-    return { status: "ERROR" };
-  }
+  } catch (err) { return { status: "ERROR" }; }
 };
